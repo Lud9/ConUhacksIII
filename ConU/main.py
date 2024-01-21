c1b64326d94677c088824dcd31ed17c29bd9586e
sakil0518 <62127364+sakil0518@users.noreply.github.com>
2024-01-21 10:21:30 -0500

    Update main.py

--- ConU/main.py
+++ ConU/main.py
@@ -5,12 +5,10 @@ import streamlit.components.v1 as components
 st.set_page_config(
     page_title="Intro to ConUGPT",
     page_icon="👋",
-    layout='wide'
+    layout='wide',
+    initial_sidebar_state="expanded"
 )
 
-
-# bigcol1, bigcol2 = st.columns([3,1])
-# with bigcol1:
 
 st.title('Welcome to our ConUGPT!')
 st.markdown('''
@@ -40,10 +38,11 @@ st.divider()
 st.markdown("### :thumbsup: We are here to help you!")
 
 st.markdown("#### :question: How does it work?")
-st.write("W have feeded AI all the data of classes and professors. ...")
+st.write("We used OpenAI API playground and GPT store to create the chatbox. Then, we feed the data of Concordia. Voila! Here is the expert of Concordia!")
 
 st.markdown("#### :question:  What kind of questions can I ask?")
 st.write("Below are the examples of the commands:")
+
 with st.chat_message("user"):
     st.write("Who are the teachers of comp 233? Who has the best rate among them?")
 with st.chat_message("ai"):
@@ -69,11 +68,14 @@ with st.chat_message("ai"):
     st.write("Concordia University was founded on August 24, 1974.")
 st.markdown("")
 st.markdown("#### :exclamation: Increase the accuracy")
-st.info('''change your prompt in case you suspect you did not get a complete response
+st.info('''Change your prompt in case you suspect you did not get a complete response
             Example: write "who are the teachers for soen 287?"
                     instead of "who teaches soen 287?"
 ''')
+st.info('Please try removing the white space after the command.')
 st.markdown("")
+
+
 
 html_string = '''
 <!DCTYPE html>
@@ -107,10 +109,11 @@ html_string = '''
     </script>
 </head>
 <body>
-    <div class="left-column">
-        <h1>Ask Here!</h1>
-        <!-- Your existing content for the left column -->
+    <div>
+        <h1 style="font-family: sans-serif">Ask Here!</h1>
     </div>
+    <span style='font-size:50px;'>&#8595;</span>
+
 </body>
 </html>
 '''
@@ -118,11 +121,16 @@ html_string = '''
 with st.sidebar:
     components.html(html_string, height=705)
 
+st.header(":arrow_left: Now open the sidebar to start!")
+st.text("You can always adjust the size of the sidebar")
 
-# with bigcol2:
-#     components.html(html_string, height=800)
+st.markdown("")
+st.markdown("")
+st.markdown("")
+with st.expander("Image credit"):
+    st.write('"https://www.freepik.com/free-vector/student-stress-concept-illustration_27828937.htm#query=busy&position=25&from_view=search&track=sph&uuid=a92733f7-98da-4176-b339-1dc45a007a0d"')
+    st.write('"https://www.freepik.com/free-vector/customer-survey-concept-illustration_5841882.htm#from_view=detail_author"')
+    st.write('"https://www.freepik.com/free-vector/search-concept-illustration_4957155.htm#page=3&position=2&from_view=author&uuid=bf2feb3f-fb5a-417f-b3e1-cc9452fb97ac"')
+    st.write('Images by storyset on Freepik')
 
-
-st.header("Now open the sidebar to start!")
 # st.link_button("Click here to try ConUGPT!", "https://conugpt.netlify.app/", type='primary')
-
